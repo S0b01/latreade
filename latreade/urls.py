8baa024ea097ed358386a7a9432ad8dce585d69b
latreade.1 <dimasobolevsky@yandex.ru>
2024-07-15 20:56:13 +0500

    SQLlite

--- latreade/urls.py
+++ latreade/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls', namespace='main'))
-
+    path('', include('main.urls', namespace='main')),
+    path('catalog/', include('goods.urls', namespace='catalog'))
 ]
